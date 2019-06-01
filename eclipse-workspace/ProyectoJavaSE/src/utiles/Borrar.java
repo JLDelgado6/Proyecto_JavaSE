9eca43fa8ce304f154e92844b8628ee77c3752f5
usuario <usuario@192.168.1.14>
2019-06-02 01:14:20 +0200

    try-catch añadido a Borrar. Debo corregir fallo cuando el tamaño es 1

--- eclipse-workspace/ProyectoJavaSE/src/utiles/Borrar.java
+++ eclipse-workspace/ProyectoJavaSE/src/utiles/Borrar.java
@@ -19,11 +19,21 @@ public class Borrar {
 		 */
 		//
 		// lanzar lista
-		utiles.Listar.listar();
-		System.out.println(utiles.GestList.lstVehiculos);
-		Integer	vb = utiles.LecturaTeclado.lecturaEntero("* Pulsar elemento que desea borrar. ");
-		Vehiculo vehBorrado = utiles.GestList.lstVehiculos.get(vb);	
-			utiles.GestList.lstVehiculos.remove(vehBorrado);
+		Integer vb;
+		try {
+			do {
+				utiles.Listar.listar();
+				System.out.println(utiles.GestList.lstVehiculos);
+				vb = utiles.LecturaTeclado.lecturaEntero("* Pulsar elemento que desea borrar. ");
+				Vehiculo vehBorrado = utiles.GestList.lstVehiculos.get(vb);	
+				utiles.GestList.lstVehiculos.remove(vehBorrado);
+			}while(vb >=0 && vb < utiles.GestList.lstVehiculos.size());
+			if(utiles.GestList.lstVehiculos.size()==0) {
+				System.out.println("La lista seleccionada está vacía"); //Corregir
+				start.App.main(null);
+			};
+			}catch(Exception e) {
+			}finally {System.out.println("No es un tipo de dato permitido");};
 		}
 	}
 
