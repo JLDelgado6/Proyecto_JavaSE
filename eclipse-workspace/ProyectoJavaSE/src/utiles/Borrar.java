5875557df67b02d5a5dba5dd14a145178d8f16ae
JLDelgado6 <usuario@192.168.1.14>
2019-06-03 00:54:47 +0200

    Borrar definitivo y documentado

--- eclipse-workspace/ProyectoJavaSE/src/utiles/Borrar.java
+++ eclipse-workspace/ProyectoJavaSE/src/utiles/Borrar.java
@@ -9,42 +9,40 @@ import tierra.Camion;
 import tierra.Coche;
 
 /**
- * @author José Luis Clase Borrar. Permite borrar vehículos listados en el
- *         sistema.
+ * @author José Luis 
+ * Clase Borrar. Permite borrar vehículos listados en el sistema.
  * @version 03/06/2019
  */
+
 @SuppressWarnings("unused")
 public class Borrar {
 
-	public static void borrar() {
-
-		int opcion = 0;
-		do {
-			try {mostrarMenu();
-				opcion = utiles.LecturaTeclado.lecturaEntero("* Pulsar 0 para salir. ");
-				ejecutarOpcion(opcion);
-			} catch (Exception e1) {
-				System.out.println("XXX No es un tipo de dato permitido XXX");
-			}
-		} while (opcion != 0);
-	}
-
-	/*
-	 * int opcion = 0; String salida;
+	/**
+	 * @author José Luis 
+	 * Clase Borrar. Pregunta primero si se quiere volver atrás o se quiere borrar un vehículo
+	 * Cuando se elige borrar, vuelve al menú principal si la lista está vacía, y pide el vehículo a borrar si no.
+	 * Saltan excepciones cuando se introducen caracteres no válidos: valores no numéricos y valores numéricos superiores al índice máximo
+	 * @version 03/06/2019
 	 */
-	//
-	// lanzar lista
-	private static void ejecutarOpcion(int opcion) {
-		System.out.println("**********************************");
-		switch(opcion) {
-	    case 1: 
+ public static void borrar() {
+		//
+		// lanzar lista
+	 int opcion = 0;
+	 
 		Integer vb;
-		Integer tamanoListaMod = utiles.GestList.lstVehiculos.size() + 1;
+		Integer tamanoListaMod = utiles.GestList.lstVehiculos.size()+1;
 		try {
+			opcion = utiles.LecturaTeclado.lecturaEntero("* Pulsar 0 para volver, 1 para borrar. *");
+			switch(opcion) {
+			case 0:
+				start.App.main(null);
+				break;
+			case 1:
 			do {
 				vb = 0;
 				if (utiles.GestList.lstVehiculos.size() == 0) {
 					System.out.println("La lista seleccionada está vacía");
+					start.App.main(null);
 				} else {
 					utiles.Listar.listar();
 					// System.out.println(utiles.GestList.lstVehiculos);
@@ -52,23 +50,17 @@ public class Borrar {
 					Vehiculo vehBorrado = utiles.GestList.lstVehiculos.get(vb - 1);
 					utiles.GestList.lstVehiculos.remove(vehBorrado);
 					utiles.Listar.listar();
-					}		
+					start.App.main(null);
+				}
 			} while (vb >= 0 && vb < tamanoListaMod);
-			opcion=0;
+			break;
+			} 
 		} catch (Exception e) {
 		} finally {
 			System.out.println("No es un tipo de dato permitido");
 		}
-		
+		;
 	}
-}
-	private static void mostrarMenu() {
-		System.out.println("******************");
-		System.out.println("* MENU BORRADO *");
-		System.out.println("**********************************");
-		System.out.println("*Opción 0. Salir    			 *");
-		System.out.println("*Opción 1. Borrar un vehículo    *");
-		System.out.println("**********************************");
 
-	}
+
 }
