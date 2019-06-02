5d8862888e6f4b1840bdba5bfaee73377a453722
usuario <usuario@192.168.1.14>
2019-06-03 00:07:31 +0200

    Borrar arreglado

--- eclipse-workspace/ProyectoJavaSE/src/utiles/Borrar.java
+++ eclipse-workspace/ProyectoJavaSE/src/utiles/Borrar.java
@@ -8,7 +8,6 @@ import start.Vehiculo;
 import tierra.Camion;
 import tierra.Coche;
 
-
 @SuppressWarnings("unused")
 public class Borrar {
 
@@ -20,23 +19,28 @@ public class Borrar {
 		//
 		// lanzar lista
 		Integer vb;
-		Integer tamanoListaMod = utiles.GestList.lstVehiculos.size()+1;
+		Integer tamanoListaMod = utiles.GestList.lstVehiculos.size() + 1;
 		try {
 			do {
-				utiles.Listar.listar();
-				//System.out.println(utiles.GestList.lstVehiculos);
-				vb = utiles.LecturaTeclado.lecturaEntero("* Pulsar elemento que desea borrar. ");
-				Vehiculo vehBorrado = utiles.GestList.lstVehiculos.get(vb-1);	
-				utiles.GestList.lstVehiculos.remove(vehBorrado);
-			}while(vb >=0 && vb < tamanoListaMod);
-			
-			if(utiles.GestList.lstVehiculos.size()==0) {
-				System.out.println("La lista seleccionada está vacía"); //Corregir
-				start.App.main(null);
-			};
-			}catch(Exception e) {
-			}finally {System.out.println("No es un tipo de dato permitido");};
+				vb = 0;
+				if (utiles.GestList.lstVehiculos.size() == 0) {
+					System.out.println("La lista seleccionada está vacía");
+					start.App.main(null);
+				} else {
+					utiles.Listar.listar();
+					// System.out.println(utiles.GestList.lstVehiculos);
+					vb = utiles.LecturaTeclado.lecturaEntero("* Pulsar elemento que desea borrar. ");
+					Vehiculo vehBorrado = utiles.GestList.lstVehiculos.get(vb - 1);
+					utiles.GestList.lstVehiculos.remove(vehBorrado);
+					utiles.Listar.listar();
+					start.App.main(null);
+				}
+			} while (vb >= 0 && vb < tamanoListaMod);
+
+		} catch (Exception e) {
+		} finally {
+			System.out.println("No es un tipo de dato permitido");
 		}
+		;
 	}
-
-
+}
