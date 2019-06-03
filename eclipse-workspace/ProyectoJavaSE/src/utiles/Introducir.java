3f260dd0e9c5909c11f52eb394d2d36537de63ff
seba9 <seba9@LAPTOP-NNSDL1NG>
2019-06-03 13:54:25 +0200

    Modificaciones de introducir para referenciar mejor

--- eclipse-workspace/ProyectoJavaSE/src/utiles/Introducir.java
+++ eclipse-workspace/ProyectoJavaSE/src/utiles/Introducir.java
@@ -43,9 +43,9 @@ public class Introducir {
 			    String b1 = utiles.LecturaTeclado.lecturaString("- Marca: ");
 			    String c1 = utiles.LecturaTeclado.lecturaString("- Modelo: ");
 			    Integer d1 = utiles.LecturaTeclado.lecturaEntero("- Números de Puertas(de 2 a 7): ");
-			    if (d1<2 || d1>7) {System.out.println("XXX El Número de Puertas no es correcto XXX");break;}
+			    if (d1<2 || d1>7) {System.out.println("XXX El Número de Puertas no es correcto XXX");ejecutarOpcion(1);}
 			    Integer e1 = utiles.LecturaTeclado.lecturaEntero("- Volumen del Maletero (dm^3): ");
-			    if (e1 < 0 || e1>1000) {System.out.println("XXX El Volumen de Maletero no es correcto XXX");break;}
+			    if (e1 < 0 || e1>1000) {System.out.println("XXX El Volumen de Maletero no es correcto XXX");ejecutarOpcion(1);}
 			    TpCAR f1 = utiles.LecturaTeclado.lecturaTpCAR("- Tipo de Carnet: ");
 			    TpCOM g1 = utiles.LecturaTeclado.lecturaTpCOM("- Tipo de Combustiple: ");
 			    
@@ -53,6 +53,7 @@ public class Introducir {
 			    
 	    		} catch(Exception e1) {
 	    			System.out.println("XXX No es un tipo de dato permitido XXX");
+	    			ejecutarOpcion(1);
 	    		}
 	    		break;
 	    		
@@ -63,15 +64,16 @@ public class Introducir {
 			    String b2 = utiles.LecturaTeclado.lecturaString("- Marca: ");
 			    String c2 = utiles.LecturaTeclado.lecturaString("- Modelo: ");
 			    Integer d2 = utiles.LecturaTeclado.lecturaEntero("- Tara(Kg): ");
-			    if (d2 < 500 || d2 > 31000) {System.out.println("XXX La Tara no es correcta XXX");break;}
+			    if (d2 < 500 || d2 > 31000) {System.out.println("XXX La Tara no es correcta XXX");ejecutarOpcion(2);}
 			    Integer e2 = utiles.LecturaTeclado.lecturaEntero("- Número de Ejes: ");
-			    if (e2 < 1 || e2 > 4) {System.out.println("XXX El Número de Ejes no es correcto XXX");break;}
+			    if (e2 < 1 || e2 > 4) {System.out.println("XXX El Número de Ejes no es correcto XXX");ejecutarOpcion(2);}
 			    TpCAR f2 = utiles.LecturaTeclado.lecturaTpCAR("- Tipo de Carnet: ");
 			    TpCOM g2 = utiles.LecturaTeclado.lecturaTpCOM("- Tipo de Combustiple: ");
 			    
 			    utiles.GestList.lstVehiculos.add( new Camion(a2,b2,c2,g2,d2,e2,f2));
 				} catch(Exception e1) {
 	    			System.out.println("XXX No es un tipo de dato permitido XXX");
+	    			ejecutarOpcion(2);
 	    		}
 	    		break;
 	    		
@@ -80,11 +82,11 @@ public class Introducir {
 				System.out.println("Introduzca los siguientes Datos:");
 			    String a3 = utiles.LecturaTeclado.lecturaString("- Número de Bastidor: ");
 			    Integer b3 = utiles.LecturaTeclado.lecturaEntero("- Altura Máxima de Vuelo (m): ");
-			    if (b3 < 0 || b3 > 1000) {System.out.println("XXX La Altura Máxima de Vuelo no es correcta XXX");break;}
+			    if (b3 < 0 || b3 > 1000) {System.out.println("XXX La Altura Máxima de Vuelo no es correcta XXX");ejecutarOpcion(3);}
 			    Integer c3 = utiles.LecturaTeclado.lecturaEntero("- Kilometro de Autonomía (km) ");
-			    if (c3 < 0 || c3 > 10000000) {System.out.println("XXX El kilometro de Autonomía no es correcto XXX");break;}
+			    if (c3 < 0 || c3 > 10000000) {System.out.println("XXX El kilometro de Autonomía no es correcto XXX");ejecutarOpcion(3);}
 			    Integer d3 = utiles.LecturaTeclado.lecturaEntero("- Potencia (kV)-(min 10kV) ");
-			    if (d3 < 10  || d3 > 50000) {System.out.println("XXX La Potencia no es correcta XXX");break;}
+			    if (d3 < 10  || d3 > 50000) {System.out.println("XXX La Potencia no es correcta XXX");ejecutarOpcion(3);}
 			    TpCAM f3 = utiles.LecturaTeclado.lecturaTpCAM("- Tipo de Cámara: ");
 			    TpCAR g3 = utiles.LecturaTeclado.lecturaTpCAR("- Tipo de Combustiple: ");
 			    
@@ -92,6 +94,7 @@ public class Introducir {
 			    
 				} catch(Exception e1) {
 	    			System.out.println("XXX No es un tipo de dato permitido XXX");
+	    			ejecutarOpcion(3);
 	    		}
 	    		break;
 	    		
@@ -100,18 +103,19 @@ public class Introducir {
 				System.out.println("Introduzca los siguientes Datos:");
 			    String a4 = utiles.LecturaTeclado.lecturaString("- Número de Bastidor: ");
 			    Integer b4 = utiles.LecturaTeclado.lecturaEntero("- Altura Máxima de Vuelo (m): ");
-			    if (b4 < 0 || b4 > 12800) {System.out.println("XXX La Altura Máxima de Vuelo no es correcta XXX");break;}
+			    if (b4 < 0 || b4 > 12800) {System.out.println("XXX La Altura Máxima de Vuelo no es correcta XXX");ejecutarOpcion(4);}
 			    Integer c4 = utiles.LecturaTeclado.lecturaEntero("- Kilometro de Autonomía ");
-			    if (c4 < 0) {System.out.println("XXX El kilometro de Autonomía no es correcto XXX");break;}
+			    if (c4 < 0) {System.out.println("XXX El kilometro de Autonomía no es correcto XXX");ejecutarOpcion(4);}
 			    Integer d4 = utiles.LecturaTeclado.lecturaEntero("- Máximo Número de Pasajeros:  ");
-			    if (d4 < 5 || d4 > 550) {System.out.println("XXX El Máximo Número de Pasajeros no es correcto XXX");break;}
+			    if (d4 < 5 || d4 > 550) {System.out.println("XXX El Máximo Número de Pasajeros no es correcto XXX");ejecutarOpcion(4);}
 			    Integer e4 = utiles.LecturaTeclado.lecturaEntero("- Máximo Número de Turbinas: (entre 2 y 6)  ");
-			    if (e4 < 2 || e4 > 6) {System.out.println("XXX El Máximo Número de Turbinas no es correcto XXX");break;}
+			    if (e4 < 2 || e4 > 6) {System.out.println("XXX El Máximo Número de Turbinas no es correcto XXX");ejecutarOpcion(4);}
 			    
 			    utiles.GestList.lstVehiculos.add( new Avion (a4,b4,c4,d4,e4));
 			       
 				} catch(Exception e1) {
 	    			System.out.println("XXX No es un tipo de dato permitido XXX");
+	    			ejecutarOpcion(4);
 	    		}
 	    		break;
 			case 0: break;
